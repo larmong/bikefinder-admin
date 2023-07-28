70b6ceb9b6790a781b8562952784a03bd3916de4
larmong <larmong@naver.com>
2023-07-28 22:31:03 +0900

    [ADD] member

--- src/components/units/user/member/Member.container.tsx
+++ src/components/units/user/member/Member.container.tsx
@@ -1,3 +1,14 @@
+import * as S from "../../dashboard/Dashboard.style";
+
 export default function Member() {
-  return <>Member</>;
+  return (
+    <S.Wrapper>
+      <S.Contents>
+        <S.Title>
+          <span>일반회원</span>
+        </S.Title>
+        <S.Content>{/*<DashboardFaq boardData={fetchBoard} />*/}</S.Content>
+      </S.Contents>
+    </S.Wrapper>
+  );
 }
