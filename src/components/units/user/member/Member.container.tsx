c1bb91bfb39c9761bf7bd81664738550ca0f4d10
larmong <larmong@naver.com>
2023-07-29 22:35:58 +0900

    [ADD] member board

--- src/components/units/user/member/Member.container.tsx
+++ src/components/units/user/member/Member.container.tsx
@@ -1,4 +1,5 @@
 import * as S from "../../dashboard/Dashboard.style";
+import DashboardFaq from "../../dashboard/board/Board.container";
 
 export default function Member() {
   return (
@@ -7,7 +8,9 @@ export default function Member() {
         <S.Title>
           <span>일반회원</span>
         </S.Title>
-        <S.Content>{/*<DashboardFaq boardData={fetchBoard} />*/}</S.Content>
+        <S.Content>
+          <DashboardFaq boardData={fetchBoard} />
+        </S.Content>
       </S.Contents>
     </S.Wrapper>
   );
