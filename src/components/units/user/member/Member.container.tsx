afac9553e55a7c3aba601a395750568eb405d88e
larmong <larmong@naver.com>
2023-07-30 23:40:40 +0900

    [FIX] member board

--- src/components/units/user/member/Member.container.tsx
+++ src/components/units/user/member/Member.container.tsx
@@ -1,7 +1,32 @@
 import * as S from "../../dashboard/Dashboard.style";
 import DashboardFaq from "../../dashboard/board/Board.container";
+import { useEffect, useState } from "react";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { db } from "../../../../commons/libraries/firebase/firebase.config";
+import { IFetchFaq } from "../../dashboard/Dashboard.types";
+import { useRouter } from "next/router";
 
 export default function Member() {
+  const router = useRouter();
+  const [fetchBoard, setFetchBoard] = useState<IFetchFaq[]>([]);
+
+  useEffect(() => {
+    const getBoardData = async () => {
+      try {
+        const boardCollection = collection(db, "user");
+        let boardQuery = query(boardCollection, orderBy("date", "desc"));
+        const data = await getDocs(boardQuery);
+        const result = data.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setFetchBoard(result);
+      } catch (error) {}
+    };
+    void getBoardData();
+  }, []);
+
   return (
     <S.Wrapper>
       <S.Contents>
