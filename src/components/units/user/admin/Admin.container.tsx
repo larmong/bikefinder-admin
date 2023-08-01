0db12d353d1598e81b79d3b3d4465e74f55918d5
larmong <larmong@naver.com>
2023-08-01 22:51:26 +0900

    [ADD] admin user

--- src/components/units/user/admin/Admin.container.tsx
+++ src/components/units/user/admin/Admin.container.tsx
@@ -1,3 +1,84 @@
-export default function Admin() {
-  return <>Admin</>;
+import * as S from "../../dashboard/Dashboard.style";
+import MemberBoard from "./board/Board.container";
+import { useEffect, useState } from "react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { db } from "../../../../commons/libraries/firebase/firebase.config";
+import { IFetchMember } from "./board/Board.types";
+import { CustomMouseEvent } from "../../../../commons/types/global.types";
+
+export default function Member() {
+  const USER_STATE_TYPE = [
+    {
+      id: 0,
+      name: "전체",
+      checkedState: true,
+    },
+    {
+      id: 1,
+      name: "정상",
+      checkedState: false,
+    },
+    {
+      id: 2,
+      name: "정지",
+      checkedState: false,
+    },
+  ];
+
+  const [fetchBoard, setFetchBoard] = useState<IFetchMember[]>([]);
+  const [filteredBoard, setFilteredBoard] = useState<IFetchMember[]>([]);
+  const [userStateType, setUserStateType] = useState<number>(0);
+
+  const onClickUserDetail = (event: CustomMouseEvent) => {
+    console.log(event.currentTarget.id);
+  };
+
+  useEffect(() => {
+    const getBoardData = async () => {
+      try {
+        const data = await query(
+          collection(db, "user"),
+          where("level", "==", 0),
+          orderBy("date", "desc")
+        );
+        const getData = await getDocs(data);
+        const result: any = getData.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setFetchBoard(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    void getBoardData();
+  }, []);
+
+  useEffect(() => {
+    let filteredData = fetchBoard;
+    if (userStateType === 1) {
+      filteredData = filteredData.filter((item: IFetchMember) => item.state);
+    } else if (userStateType === 2) {
+      filteredData = filteredData.filter((item: IFetchMember) => !item.state);
+    }
+    setFilteredBoard(filteredData);
+  }, [fetchBoard, userStateType]);
+
+  return (
+    <S.Wrapper>
+      <S.Contents>
+        <S.Title>
+          <span>일반회원</span>
+        </S.Title>
+        <S.Content>
+          <MemberBoard
+            boardData={filteredBoard}
+            USER_STATE_TYPE={USER_STATE_TYPE}
+            setUserStateType={setUserStateType}
+            onClickUserDetail={onClickUserDetail}
+          />
+        </S.Content>
+      </S.Contents>
+    </S.Wrapper>
+  );
 }
